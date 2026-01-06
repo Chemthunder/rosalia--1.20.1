cb4269da4302fe3f98285f5290c8362f44d4410c
Chemthunder <chemthunderisme@gmail.com>
2026-01-06 17:49:52 -0500

    ok im done today

--- src/main/java/silly/chemthunder/rosalia/index/RosaliaItems.java
+++ src/main/java/silly/chemthunder/rosalia/index/RosaliaItems.java
@@ -1,11 +1,14 @@
 package silly.chemthunder.rosalia.index;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.MusicDiscItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import silly.chemthunder.rosalia.Rosalia;
 import silly.chemthunder.rosalia.item.BlossomingLongswordItem;
+import silly.chemthunder.rosalia.item.FloweringVesselItem;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -15,6 +18,15 @@ public interface RosaliaItems {
 
     Item BLOSSOMING_LONGSWORD = create("blossoming_longsword", new BlossomingLongswordItem(RosaliaToolMaterials.LONGSWORD, 8, -2.8f, new Item.Settings()
             .maxCount(1)
+    ));
+
+    Item FLOWERING_VESSEL = create("flowering_vessel", new FloweringVesselItem(new Item.Settings()
+            .maxCount(1)
+    ));
+
+    Item DAISY_BELL = create("daisy_bell", new MusicDiscItem(4, SoundEvents.BLOCK_COPPER_BREAK, new  Item.Settings()
+            .maxCount(1)
+            , 120
     ));
 
     static <T extends Item> T create(String name, T item) {
---
+++ src/main/java/silly/chemthunder/rosalia/item/FloweringVesselItem.java
@@ -0,0 +1,9 @@
+package silly.chemthunder.rosalia.item;
+
+import net.minecraft.item.Item;
+
+public class FloweringVesselItem extends Item {
+    public FloweringVesselItem(Settings settings) {
+        super(settings);
+    }
+}
